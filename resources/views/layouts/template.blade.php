af55ddf4f30bebf57ae731dd8b31d4f34d82a6ae
AdeMariaUlfa <ademariaulfaa@gmail.com>
2023-03-27 11:07:42 +0700

    add text pengembang

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -183,14 +183,14 @@
         @yield('content')
     </div>
 
-
-
-
-
+        
     <footer  style=" background: -webkit-linear-gradient(to right, #a3e2df 50%, #effffe 100%);  background-color: rgba(51,204,197,255); background: -moz-linear-gradient(to right, #a3e2df 0%, #effffe 100%);  background: linear-gradient(to right, #a3e2df 0%, #effffe 100%);">
         <div class="container" >
             <div class="row">
                 <div class="col-lg-12">
+                    <p>
+                        Pengembang : Mia Andria Ningsih & Bayu Kurniawan, M.Pd. 
+                    </p>
                     <p>
                         <img src="{{ asset('images/um.webp') }}" style="width:30px; height: 30px;">
                         Copyright © 2023. All Rights Reserved.
